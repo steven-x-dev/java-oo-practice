b7a5e54f9281d290ff64173627069e5c58664142
steven-x-dev <steven-dev@qq.com>
2020-09-09 01:13:19 +0800

    [steven] feat: added the CRUD API for the hot search module, including HotSearchService and HotSearchController

---
+++ src/com/twu/controller/HotSearchController.java
@@ -0,0 +1,186 @@
+package com.twu.controller;
+
+import com.twu.entity.HotSearch;
+import com.twu.entity.RegularUser;
+import com.twu.entity.User;
+import com.twu.service.HotSearchService;
+import com.twu.service.SessionManager;
+import com.twu.service.UserService;
+
+import java.util.List;
+
+
+/**
+ * The HotSearchController is a singleton that processes user requests by calling
+ * corresponding services to access the data. Each method in the controller is a
+ * transaction
+ */
+public class HotSearchController {
+
+
+    /**
+     * The singleton instance
+     */
+    private static HotSearchController instance = new HotSearchController();
+
+
+    /**
+     * The HotSearchService instance used to access hot search data
+     */
+    private HotSearchService hotSearchService = HotSearchService.getInstance();
+
+
+    /**
+     * The UserService instance used to access user data
+     */
+    private UserService userService = UserService.getInstance();
+
+
+    /**
+     * The SessionManager instance used to retrieve the requesting user's session
+     */
+    private SessionManager sessionManager = SessionManager.getInstance();
+
+
+    /**
+     * @see HotSearchService#exists(String)
+     */
+    public boolean exists(String name) {
+        return hotSearchService.exists(name);
+    }
+
+
+    /**
+     * @see HotSearchService#getCount()
+     */
+    public int getCount() {
+        return hotSearchService.getCount();
+    }
+
+
+    /**
+     * @see HotSearchService#findOneByName(String)
+     */
+    public HotSearch findOneByName(String name) {
+        return hotSearchService.findOneByName(name);
+    }
+
+
+    /**
+     * @see HotSearchService#findAll()
+     */
+    public List<HotSearch> findAll() {
+        return hotSearchService.findAll();
+    }
+
+
+    /**
+     * @see HotSearchService#getIndexByName(String)
+     */
+    public int getIndexByName(String name) {
+        return hotSearchService.getIndexByName(name);
+    }
+
+
+    /**
+     * Add a hot search to the hot search list.
+     * Perform a check of the existence of the hot search name to
+     * be added in existing hot searches, and add a new hot search
+     * with this name if it doesn't exist
+     *
+     * @param name the name of the hot search to be added
+     *
+     * @return true  if the hot search name does not exist and the
+     *               hot search has been successfully added, or
+     *         false if the hot search name exists and the request
+     *               has been refused
+     *
+     * @see HotSearchService#addHotSearch(String)
+     */
+    public boolean addHotSearch(String name) {
+
+        HotSearch existing = hotSearchService.findOneByName(name);
+
+        if (existing != null)
+            return false;
+
+        hotSearchService.addHotSearch(name);
+
+        return true;
+    }
+
+
+    /**
+     * @see HotSearchService#addSuperHotSearch(String)
+     * @see HotSearchController#addHotSearch(String)
+     */
+    public boolean addSuperHotSearch(String name) {
+
+        HotSearch existing = hotSearchService.findOneByName(name);
+
+        if (existing != null)
+            return false;
+
+        hotSearchService.addSuperHotSearch(name);
+
+        return true;
+    }
+
+
+    /**
+     * @see HotSearchService#buyHotSearch(String, int, int)
+     */
+    public int buyHotSearch(String name, int rank, int amount) {
+        return hotSearchService.buyHotSearch(name, rank, amount);
+    }
+
+
+    /**
+     * Subtract a certain amount of votes from the remaining votes of the
+     * regular user identified by the username, and votes to the hot search
+     * identified by the name.
+     *
+     * @param name the name of the hot search
+     * @param username the username of the requesting user
+     * @param votes the number of votes from the user to be added to the
+     *              hot search
+     *
+     * @return true  if the transaction is successful, or
+     *         false if the user with the username doesn't exist, or the
+     *               user exists but hasn't logged in, or other cases
+     *               returning false from UserService#useVotes(String, int)
+     *               or HotSearchService#voteHotSearch(String, int)
+     *
+     * @see HotSearchService#voteHotSearch(String, int)
+     * @see UserService#findOneRegularUserByUsername(String)
+     * @see UserService#useVotes(String, int)
+     * @see SessionManager#getActiveSession(User)
+     */
+    public boolean voteHotSearch(String name, String username, int votes) {
+
+        RegularUser authenticatedUser = userService.findOneRegularUserByUsername(username);
+
+        if (authenticatedUser == null ||
+                sessionManager.getActiveSession(authenticatedUser) == null) {
+            return false;
+        }
+
+        return userService.useVotes(username, votes) &&
+                hotSearchService.voteHotSearch(name, votes);
+    }
+
+
+    /**
+     * Do not let this class to be instantiated externally
+     */
+    private HotSearchController() {}
+
+
+    /**
+     * Return the singleton instance of HotSearchController
+     */
+    public static HotSearchController getInstance() {
+        return instance;
+    }
+
+}
---
+++ src/com/twu/service/HotSearchService.java
@@ -0,0 +1,270 @@
+package com.twu.service;
+
+import com.twu.entity.HotSearch;
+import com.twu.entity.SuperHotSearch;
+import com.twu.utility.StreamUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+
+/**
+ * The HotSearchService is a singleton that manages a list of hot searches.
+ * It accesses the hot search data and manages the CRUD operations of the data
+ */
+public class HotSearchService {
+
+
+    /**
+     * The singleton instance
+     */
+    private static HotSearchService instance = new HotSearchService();
+
+
+    /**
+     * The list of hot searches
+     */
+    private List<HotSearch> hotSearches;
+
+
+    /**
+     * Check whether a hot search name already exists, case insensitive
+     *
+     * @param name the specified hot search name to be checked
+     * @return whether the specified hot search name exists
+     */
+    public boolean exists(String name) {
+        return hotSearches.stream().anyMatch(matchByName(name));
+    }
+
+
+    /**
+     * Get the count of all hot searches
+     *
+     * @return the count of all hot searches
+     */
+    public int getCount() {
+        return hotSearches.size();
+    }
+
+
+    /**
+     * Find a single hot search by the specified name, case insensitive
+     *
+     * @param name the specified hot search name
+     * @return the hot search with the specified name, or null if one
+     *         doesn't exist
+     */
+    public HotSearch findOneByName(String name) {
+        return hotSearches.stream().filter(matchByName(name)).collect(StreamUtil.limitOne());
+    }
+
+
+    /**
+     * Find all hot searches
+     *
+     * @return a copy of the list of all hot searches
+     */
+    public List<HotSearch> findAll() {
+        return new ArrayList<>(hotSearches);
+    }
+
+
+    /**
+     * Get the position of a hot search, identified by its name,
+     * in the hot search list
+     *
+     * @param name the specified hot search name
+     * @return the index of the hot search in the hot search list,
+     *         or -1 if one doesn't exist
+     */
+    public int getIndexByName(String name) {
+        for (int i = 0; i < hotSearches.size(); i++) {
+            if (hotSearches.get(i).getName().equalsIgnoreCase(name))
+                return i;
+        }
+        return -1;
+    }
+
+
+    /**
+     * Add a hot search to the hot search list.
+     * This method does not check whether the hot search name already
+     * exists, and whoever uses this method must check it beforehand
+     *
+     * @param name the name of the hot search to be added
+     */
+    public void addHotSearch(String name) {
+        hotSearches.add(new HotSearch(name));
+    }
+
+
+    /**
+     * Add a super hot search to the hot search list.
+     * This method does not check whether the hot search name already
+     * exists, and whoever uses this method must check it beforehand
+     *
+     * @param name the name of the super hot search to be added
+     */
+    public void addSuperHotSearch(String name) {
+        hotSearches.add(new SuperHotSearch(name));
+    }
+
+
+    /**
+     * Buy a given position on the hot search list for a hot search
+     * with a certain amount.
+     *
+     * If the existing hot search at the given rank is a different hot
+     * search from the one specified by the name, the current hot search
+     * will replace the original one and the original one will disappear.
+     * All hot searches after the original position of the hot search
+     * being bought will step up by one in the list
+     *
+     * @param name the name of the hot search
+     * @param rank the 1-based index on the hot search list to be bought
+     * @param amount the amount charged in this purchase
+     *
+     * @return  1 if the operation is successful, or
+     *          0 if the existing hot search at the given rank is a different
+     *               hot search from the one specified by the name, and the
+     *               amount of that hot search is equal to or greater than
+     *               the current amount, or
+     *         -1 if the hot search with the specified name doesn't exist
+     */
+    public int buyHotSearch(String name, int rank, int amount) {
+
+        int internalRank = rank - 1;
+
+        HotSearch toBuy = findOneByName(name);
+
+        if (toBuy == null)
+            return -1;
+
+        HotSearch existing = hotSearches.get(internalRank);
+
+        int newPrice = toBuy.getAmount() + amount;
+
+        if (toBuy == existing) {
+            toBuy.setAmount(newPrice);
+            return 1;
+        }
+
+        if (existing.getAmount() >= newPrice)
+            return 0;
+
+        hotSearches.remove(toBuy);
+        hotSearches.set(internalRank, toBuy);
+
+        return 1;
+    }
+
+
+    /**
+     * Add votes to the hot search identified by the name.
+     *
+     * If the voted hot search is already bought at that position,
+     * no re-ordering will take place. Otherwise, the voted one
+     * will advance its place by comparing its votes with other
+     * hot searches that have not been bought. If the hot search
+     * is moved to a new location, it is guaranteed that the one
+     * after its new location has not been bought, therefore other
+     * bought hot searches between [newIndex + 2, oldIndex] in
+     * the reordered-list have stepped back by one, and their
+     * positions must be restored
+     *
+     * @param name the name of the hot search
+     * @param votes the number of votes from the user to be added
+     * @return true  if the operation is successful, or
+     *         false if the hot search with the specified name doesn't exist
+     */
+    public boolean voteHotSearch(String name, int votes) {
+
+        HotSearch hs = null;
+        int oldIndex = -1;
+
+        for (int i = 0; i < hotSearches.size(); i++) {
+            HotSearch curr = hotSearches.get(i);
+            if (curr.getName().equalsIgnoreCase(name)) {
+                hs = curr;
+                oldIndex = i;
+                break;
+            }
+        }
+
+        if (hs == null)
+            return false;
+
+        int newVote = hs.addVotes(votes);
+
+        if (hs.getAmount() > 0)
+            return true;
+
+        int newIndex = oldIndex;
+
+        for (int i = 0; i < hotSearches.size(); i++) {
+
+            HotSearch curr = hotSearches.get(i);
+
+            if (curr.getAmount() == 0 && newVote > curr.getVote()) {
+                newIndex = i;
+                break;
+            }
+        }
+
+        if (newIndex == oldIndex)
+            return true;
+
+        hotSearches.remove(hs);
+        hotSearches.add(newIndex, hs);
+
+        for (int i = newIndex + 2; i <= oldIndex; i++) {
+            HotSearch curr = hotSearches.get(i);
+            if (curr.getAmount() > 0) {
+                HotSearch prev = hotSearches.set(i - 1, curr);
+                hotSearches.set(i , prev);
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Returns a predicate that checks whether the hot search matches the
+     * specified hot search name by its name, case insensitive
+     *
+     * @param name the specified hot search name to be checked
+     * @return the predicate that checks whether the hot search matches the name
+     */
+    private static Predicate<HotSearch> matchByName(String name) {
+        return hs -> hs.getName().equalsIgnoreCase(name);
+    }
+
+
+    /**
+     * Do not let this class to be instantiated externally
+     */
+    private HotSearchService() {
+        init();
+    }
+
+
+    /**
+     * Simulates the process of connecting to a database.
+     * Create an empty list at startup to store hot searches
+     */
+    private void init() {
+        hotSearches = new ArrayList<>();
+    }
+
+
+    /**
+     * Return the singleton instance of HotSearchService
+     */
+    public static HotSearchService getInstance() {
+        return instance;
+    }
+
+}
